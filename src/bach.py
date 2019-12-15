256d715decb0029a292008f01b0bc911dff89b1d
Nils Olsson <nilso@enosis.net>
2019-12-15 01:55:11 -0800

    Holder variation plots roughly done, two Bach melodies added

--- src/bach.py
+++ src/bach.py
@@ -1,7 +1,108 @@
-import mido
+import numpy as np
+from matplotlib import pyplot as plt
 
-messages = mido.MidiFile('music/bwv1009_bourree.mid')
-notes = [n for n in messages if type(n) is mido.Message]
+from melody import Melody, pitch_intervals, rhythmic_intervals
+from bach_bwv816 import notes as bwv816 # Gavotte
+from bach_bwv1009 import notes as bwv1009 # Bourree
 
-for n in notes:
-    print(n)
+#! Sequence-to-mask helper
+def mask(indices):
+    res = np.array([False] * (indices[-1]+1))
+    for i in indices:
+        res[i] = True
+    return res
+
+#! Covering count $p_i(r)$
+def p_i(mask, i, r):
+    a = max(i - r, 0)
+    b = min(i + r, len(mask))
+    return sum(mask[a:b])
+
+#! Calculate scaling for fixed i
+def scaling(mask, i=None):
+    i = i or len(mask) // 2
+    # "to avoid the boundary effect (since the sequence is finite in length),
+    # the largest radius of the box is limited to 1/10 of the total length of
+    # the sequence"
+    rvals = np.arange(2, len(mask) // 10, dtype=int)
+    pvals = np.array([p_i(mask, i, r) for r in rvals])
+
+    log2_rvals = np.ma.log2(rvals)
+    log2_pvals = np.ma.log2(pvals)
+
+    # Get cutoff for linear regression
+    j = len(log2_pvals)
+    while j > 2 and (log2_pvals[j-2] == log2_pvals[1]):
+        j -= 1
+
+    # Slope and intercept where slope is alpha the scaling
+    if j > 1:
+        z = np.polyfit(log2_rvals[:j], log2_pvals[:j], 1)
+    else:
+        z = np.polyfit(log2_rvals, log2_pvals, 1)
+
+    return z, i, log2_rvals, log2_pvals, j
+
+#! Plot scaling for fixed i
+def plot_scaling(mask, s, i=None):
+    z, i, log2_rvals, log2_pvals, j = scaling(mask, i)
+    a, _ = z
+
+    # Linear regression
+    tvals = np.linspace(log2_rvals[0], log2_rvals[-1])
+    f = np.poly1d(z)
+    plt.plot(tvals, f(tvals), '--r', label=fr'$\alpha={a:.5}$')
+
+    # Points
+    plt.scatter(log2_rvals[:j], log2_pvals[:j], s=2**3, c='b', zorder=3,
+            label='included in regression')
+    plt.scatter(log2_rvals[j:], log2_pvals[j:], s=2**3, c='k', zorder=3,
+            label='excluded from regression')
+
+    plt.title(fr'({s}) Scaling of $p_i(r)$ for box-size $r$ with $i={i}$')
+    plt.xlabel(r'$\log_2(r)$')
+    plt.ylabel(r'$\log_2(p_i(r))$')
+    plt.legend(loc='best')
+
+def plot_scaling2(mask, s):
+    n = len(mask) // 10
+    a, b = n, n*9
+    ivals = np.arange(a, b)
+    avals = [scaling(mask, i)[0][0] for i in ivals]
+
+    plt.scatter(ivals, avals, s=2**3)
+
+    plt.xlim(0, len(mask))
+    plt.ylim(0, 2)
+    plt.xlabel(r'position $i$')
+    plt.ylabel(r'$\alpha$')
+
+# ------------------------------------------------------------------------------
+
+notes = bwv816
+# notes = bwv1009
+# notes = ['c8']*600
+# notes = ['c8'] + \
+        # ['d', 'e', 'f', 'g', 'a', 'b', 'c'] * 20
+M = Melody(notes)
+
+# for n in M.notes:
+    # print(n)
+
+I = pitch_intervals(M)
+R = rhythmic_intervals(M)
+
+# print(I)
+# print(R)
+
+I_mask = mask(I)
+R_mask = mask(R)
+
+# print(I_mask)
+# print(R_mask)
+
+# i = 300
+# plot_scaling(I_mask, 'Melody', i)
+# plot_scaling(R_mask, 'Rhythm')
+plot_scaling2(I_mask, 'Melody')
+plt.show()
--- src/melody.py
+++ src/melody.py
@@ -1,78 +1,130 @@
 import numpy as np
 import re
 
-note_name_pattern = re.compile('(\w)([sf]*)')
-note_pattern = re.compile('([a-zA-Z]+),(\d+),(\d+)')
+# NOTE: Currently not handling dotted rhythms
+LY_PATTERN = re.compile(r"([a-z])(es|is)?([,'])*([0-9])*")
 
-# Note degree look-up table
-note_degree = {
-            'c'  : 0,
-            'd'  : 2,
-            'e'  : 4,
-            'f'  : 5,
-            'g'  : 7,
-            'a'  : 9,
-            'b'  : 11,
-        }
+DEGREE_MAP = {
+        'c' : 0, 'd' : 2, 'e' : 4,
+        'f' : 5, 'g' : 7, 'a' : 9, 'b' : 11 }
 
-#! Note name parser helper
-def parse_note_name(token):
-    m = note_name_pattern.match(token)
-    assert(m)
-    n, accidentals = m.groups()
-    assert((len(set(accidentals)) <= 1))
-    mod = 0 if not accidentals else 1 if accidentals[0] == 's' else -1
-    return note_degree[n], mod*len(accidentals)
+DEGREE_MAP_INV = {
+        0: 'C',  1: 'C#', 2: 'D',   3: 'D#',
+        4: 'E',  5: 'F',  6: 'F#',  7: 'G',
+        8: 'G#', 9: 'A', 10: 'A#', 11: 'B' }
 
-#! Note class
-# For a brief description of the member fields:
-# - "name" is the full name of the note (e.g. cs for C-sharp)
-# - "deg_base" is the degree of the note without accidentals
-# - "deg_shift" is the semitone difference from the accidentals
-# - "deg" is the "deg_base" plus the "deg_shift" (a.k.a the enharmonic degree)
-# - "octave" is the octave of the enharmonic degree
-# - "value" is the rhythmic value of the note (1 for whole, 4 for quarter, etc.)
+ACCIDENTAL_MAP = {
+        'es': -1,
+        'is': 1,
+        None: 0 }
+
+VALUE_MAP_INV = {
+        1: 'whole-note',
+        2: 'half-note',
+        4: 'quarter-note',
+        8: 'eighth-note',
+        16: 'sixteenth-note' }
+
+#! Note abstraction
 class Note:
-    #! Standard constructor
-    def __init__(self, name, deg_base, deg_shift, octave, value):
-        self.name = name
-        self.deg_base = deg_base
-        self.deg_shift = deg_shift
-        self.deg = (deg_base + deg_shift) % 12
-        self.octave = octave
-        self.value = value
+    def __init__(self, d, o, v):
+        self.degree = d
+        self.octave = o
+        self.value = v
+
+    @property
+    def d(self):
+        return self.degree
+
+    @property
+    def o(self):
+        return self.octave
+
+    @property
+    def v(self):
+        return self.value
+
+    #! Subtraction operator overload
+    # Returns the absolute pitch difference between notes
+    def __sub__(self, other):
+        degree_delta = self.d - other.d
+        octave_delta = self.o - other.o
+        return abs(degree_delta + octave_delta * 12)
 
     def __str__(self):
-        return '{},{},{}'.format(self.deg, self.octave, self.value)
+        return f'{DEGREE_MAP_INV[self.d]}{self.o} {VALUE_MAP_INV[self.v]}'
 
-    #! Subtraction operator
-    def __sub__(self, other):
-        return abs((self.octave-other.octave)*12+(self.deg-other.deg))
+#! Helper to get octave of note relative to another
+# @param d  Degree of note
+# @param pd Degree of previous note
+# @param po Octave of previous note
+def relative_octave(d, pd, po):
+    delta = (12 + d - pd) % 12
+    # Increment if moving across C
+    if pd + delta > 11:
+        po += 1
+    # Invert if distance is a P5 or bigger
+    if delta > 6:
+        po -= 1
+    return po
 
-    #! Parse a note string
-    # The format is '<note name>,<octave>,<rhythmic value>'
-    # See header comments for more.
-    def parse(token):
-        m = note_pattern.match(token)
-        assert(m is not None)
-        name, octave, value = m.groups()
-        deg_base, deg_shift = parse_note_name(name)
-        return Note(name, deg_base, deg_shift, int(octave), int(value))
+#! Helper to get octave shift from comma/apostrophe string
+# It's down/up an octave for however many commas/apostrophes.
+# If empty or None, returns 0.
+# @param o Comma/apostrophe string
+def octave_shift(o):
+    assert(o is None or len(set(o)) <= 1) # Don't mix commas and apostrophes!
+    return ((-1)*len(o) if o[0] is ',' else len(o)) if o else 0
 
-#! Convert melody to sequences
-# Converts a sequence of note strings to a sequence of absolute pitch
-# differences and rhythm differences.
-def melody_to_seqs(vals):
-    notes = ([Note.parse(note) for note in vals])
-    # Couple of constants
-    n = len(notes)-1
-    M = max(notes, key=lambda n: n.value).value
-    # Construct pitch sequence
-    pitch_seq = np.zeros(len(notes)-1, dtype=int)
-    for i in range(1, len(notes)-1):
-        pitch_seq[i] = notes[i]-notes[i-1]+pitch_seq[i-1]
-    # Construct rhythm sequence
-    rhythm_seq = pitch_seq.copy()
-    for i in range(1, len(notes)-1):
-        rhythm_seq[i] = rhythm_seq[i-1] + notes[i].value // M
-    return pitch_seq, rhythm_seq
+#! Melody abstraction
+class Melody:
+    #! Constructor
+    def __init__(self, notes=None):
+        self.notes = []
+        if notes:
+            for n in notes:
+                self.addly(n)
+
+    #! Add note (LilyPond relative octave notation)
+    def addly(self, s):
+        # note, accidental, octave, value:
+        n, a, o, v = LY_PATTERN.match(s).groups()
+        d = DEGREE_MAP[n] + ACCIDENTAL_MAP[a]
+
+        # If first note in melody have to establish relative first note:
+        if len(self.notes) == 0:
+            assert(v is not None)
+            o = 4 + octave_shift(o)
+            self.notes += [Note(d, o, int(v))]
+
+        # Else octave comes from previous note
+        else:
+            prev = self.notes[-1]
+            pd, po, pv = prev.d, prev.o, prev.v
+            o = relative_octave(d, pd, po) + octave_shift(o)
+            v = int(v) if v else pv
+            self.notes += [Note(d, o, v)]
+
+#! Get sequence of absolute pitch differences in melody
+# @param M Melody object
+def pitch_intervals(M):
+    assert(type(M) is Melody)
+    n = len(M.notes)-1
+    res = np.zeros(n, dtype=int)
+    for i in range(1, n):
+        res[i] = res[i-1] + (M.notes[i] - M.notes[i-1] + 1)
+    return res
+
+#! Get sequence of rhythmic value intervals in melody
+# "A shortest measure of time is first selected and a black point is marked on
+# the first position of the line." The temporal interval of a note is then the
+# number of times this smallest value divides the note's rhythmic value.
+# @param M Melody object
+def rhythmic_intervals(M):
+    assert(type(M) is Melody)
+    n = len(M.notes)-1
+    smallest_value = max(M.notes, key=lambda n: n.v).v
+    res = np.zeros(n, dtype=int)
+    for i in range(1, n):
+        res[i] = res[i-1] + (smallest_value // M.notes[i].v)
+    return res
